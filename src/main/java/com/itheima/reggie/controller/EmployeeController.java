a1a120228bc7c5321e0a6572bd1b677feea63ddc
ljsinsist20 <2593161116@qq.com>
2022-06-26 18:17:27 +0800

    03-退出功能

--- src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -1,5 +1,7 @@
 package com.itheima.reggie.controller;
 
+import cn.hutool.crypto.SecureUtil;
+import cn.hutool.crypto.digest.DigestUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.entity.Employee;
@@ -26,13 +28,30 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
     @PostMapping("/login")
-    private R handleLogin(@RequestBody Employee employee) {
+    private R login(@RequestBody Employee employee) {
         QueryWrapper<Employee> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("username", employee.getUsername());
+        //根据用户名查询
         Employee queryEmployee = employeeService.getOne(queryWrapper);
         if (queryEmployee == null) {
             return R.error("该用户不存在");
         }
-        return R.success("登陆成功");
+        //对比密码
+        employee.setPassword(DigestUtil.md5Hex(employee.getPassword()));
+        if (!employee.getPassword().equals(queryEmployee.getPassword())) {
+            return R.error("密码错误");
+        }
+        //状态识别
+        if (queryEmployee.getStatus() == 0) {
+            return R.error("该用户已被禁用");
+        }
+        // TODO request.getSession().setAttribute("employee", employee.getId())
+        return R.success(queryEmployee);
+    }
+
+
+    @PostMapping("/logout")
+    private R logout() {
+        return R.success("退出成功");
     }
 }
