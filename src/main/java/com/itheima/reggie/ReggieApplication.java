bebf80f3ba18af915498373c93a22d9b3ed8a8dd
ljsinsist20 <2593161116@qq.com>
2022-06-26 12:08:00 +0800

    00-后端骨架

---
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -0,0 +1,20 @@
+package com.itheima.reggie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: ljs
+ * @Pcakage: com.itheima.reggie.ReggieApplication
+ * @Date: 2022年06月26日 12:00
+ * @Description:
+ */
+@Slf4j
+@SpringBootApplication
+public class ReggieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class,args);
+        log.info("项目启动成功...");
+    }
+}
